f34ab0c0eb1bea76ddb569cd7b32eef007a39382
Corinthiano611 <andradeleo1006@gmail.com>
2025-07-29 10:16:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-const dino = document.getElementById("dino");
-const obstaculo = document.getElementById("obstaculo");
-let isjumping = false;
-let speed = 10;
-const acceleration = 10;
-const interval = 30;
-const gameAreaWidth = document.getElementById("gameArea").offsetWidth;
-
-function jump() {
-    if (isjumping) return;
-    isjumping = true;
-    dino.style.animation = "jump 0.5s ease-out";
-    setTimeout( () => {
-        dino.style.animation = "";
-        isjumping = false
-    }, 500);
-}
-document.addEventListener("keydown", function(event) {
-    if (event.code === "Space") {
-        jump();
-    }    
-});
-
-function moveObstaculo() {
-    let obstaculoPos = obstaculo.offsetLeft;
-    obstaculo.style.left = obstaculoPos - speed + "px";
-    if (obstaculoPos <= -20) {
-        obstaculo.style.left=gameAreaWidth + "px";
-    }
-checkCollision();
-
-}
-
-function checkCollision() {
-    const dinoRect = dino.getBoundingClientRect();
-    const obstaculoReact = obstaculo.getBoundingClientRect();
 
         if(
             dinoRect.right > obstaculoReact.left &&
